0005f2f97eb4b646cfec8cee188dec6fc3b07063
jacadzaca <vitouejj@gmail.com>
2020-03-31 20:55:01 +0200

    removed unnecessary brackets

--- src/main/kotlin/com/jacadzaca/monopoly/Player.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/Player.kt
@@ -3,6 +3,4 @@ package com.jacadzaca.monopoly
 import io.vertx.core.http.ServerWebSocket
 import io.vertx.core.http.WebSocketFrame
 
-data class Player(val name: String, val context: ServerWebSocket, val isHost: Boolean = false) {
-
-}
+data class Player(val name: String, val context: ServerWebSocket, val isHost: Boolean = false)
