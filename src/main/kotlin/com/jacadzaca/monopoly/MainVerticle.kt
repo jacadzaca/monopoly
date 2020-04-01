d9d46af152adbececac866c52cf5612297968aab
jacadzaca <vitouejj@gmail.com>
2020-04-01 21:39:31 +0200

    removed redundant handlers

--- src/main/kotlin/com/jacadzaca/monopoly/MainVerticle.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/MainVerticle.kt
@@ -2,28 +2,9 @@ package com.jacadzaca.monopoly
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
-import io.vertx.core.http.ServerWebSocket
 import io.vertx.ext.web.Router
 
 class MainVerticle : AbstractVerticle() {
-  private val rooms: Map<String, GameManager> = mapOf<String, GameManager>("jaca" to GameMangerImpl())
-
-  private fun handle(context: ServerWebSocket) {
-    val roomName: String = context.path().substring(1)
-    val gameRoom: GameManager? = rooms[roomName]
-    if (gameRoom == null) {
-      context
-        .writeFinalTextFrame("Not such room")
-        .end()
-    }
-    if (gameRoom!!.isFull()) {
-      context
-        .writeFinalTextFrame("Rooms $gameRoom full")
-        .end()
-    }
-    gameRoom.newPlayer(context)
-  }
-
   override fun start(startPromise: Promise<Void>) {
     val router : Router = Router.router(vertx)
     router.route("/").handler { request ->
@@ -38,16 +19,8 @@ class MainVerticle : AbstractVerticle() {
         .sendFile("websocket.js")
         .end()
     }
-    router.route("/room/:roomName").handler { request ->
-      val response = rooms[request.request().getParam("roomName")]?.players()?.toString() ?: "No such room"
-      request
-        .response()
-        .putHeader("content-type", "text/plain")
-        .end(response)
-    }
     vertx
       .createHttpServer()
-      .webSocketHandler(::handle)
       .requestHandler(router)
       .listen(8080) { http ->
         if (http.succeeded()) {
