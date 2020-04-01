a5b800fd5ec815d62bba6cf26e10593f7e3f2bcf
jacadzaca <vitouejj@gmail.com>
2020-04-01 22:38:39 +0200

    players state is now remebered

--- src/main/kotlin/com/jacadzaca/monopoly/GameRoomImpl.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/GameRoomImpl.kt
@@ -5,9 +5,19 @@ import java.util.Queue
 class GameRoomImpl(private val players: Queue<Player>, private val game: MonopolyLogic) : GameRoom {
   override fun executeAction(action: String): String {
     val parsedAction = action.split(" ")
+    val currentPlayer = players.peek()
+    val report: Player;
     if (parsedAction[0] == "move") {
-      return game.movePiece(parsedAction[1].toInt(), players.peek().piece).toString()
+      val moveSize = parsedAction[1].toInt()
+      report = currentPlayer.copyPlayer(piece = game.movePiece(moveSize, currentPlayer.piece))
+      nextTurn(report)
+      return report.piece.toString()
     }
     return "Wrong input"
+  }
+
+  private fun nextTurn(currentPlayer: Player) {
+    players.remove()
+    players.add(currentPlayer)
   }
 }
