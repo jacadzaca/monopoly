d6a14f335d3af858261b658a836046650d8543ae
jacadzaca <vitouejj@gmail.com>
2020-04-01 19:37:25 +0200

    removed redundant classes

--- src/main/kotlin/com/jacadzaca/monopoly/GameManager.kt
+++
@@ -1,9 +0,0 @@
-package com.jacadzaca.monopoly
-
-import io.vertx.core.http.ServerWebSocket
-
-interface GameManager {
-  fun newPlayer(context: ServerWebSocket)
-  fun players(): List<Player>
-  fun isFull(): Boolean
-}
