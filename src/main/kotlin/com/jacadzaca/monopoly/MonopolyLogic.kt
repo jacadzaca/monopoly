c37dbe7b3f6e96720122e53c81e7deef3a4dbd0b
jacadzaca <vitouejj@gmail.com>
2020-04-13 22:55:14 +0200

    removed monopolylogic

--- src/main/kotlin/com/jacadzaca/monopoly/MonopolyLogic.kt
+++
@@ -1,5 +0,0 @@
-package com.jacadzaca.monopoly
-
-interface MonopolyLogic {
-  fun movePiece(tilesCount: Int, piece: Piece): Piece
-}
--- src/main/kotlin/com/jacadzaca/monopoly/MonopolyLogicImpl.kt
+++
@@ -1,7 +0,0 @@
-package com.jacadzaca.monopoly
-
-class MonopolyLogicImpl : MonopolyLogic {
-  override fun movePiece(tilesCount: Int, piece: Piece): Piece {
-    return piece.copy(position = piece.position + tilesCount)
-  }
-}
