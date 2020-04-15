32c7f437439488374128e56b304c52404ae5f1f4
jacadzaca <vitouejj@gmail.com>
2020-04-15 19:26:24 +0200

    players now have id

--- src/main/kotlin/com/jacadzaca/monopoly/Player.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/Player.kt
@@ -2,10 +2,13 @@ package com.jacadzaca.monopoly
 
 import io.vertx.codegen.annotations.DataObject
 import io.vertx.core.json.JsonObject
+import java.util.UUID
 
 @DataObject
-data class Player(val piece: Piece) {
-  constructor(json: JsonObject) : this(Piece(position = json.getInteger("position")))
+data class Player(val id: UUID, val piece: Piece) {
+  constructor(json: JsonObject) : this(
+    UUID.fromString(json.getString("id")),
+    Piece(position = json.getInteger("position")))
 
   fun toJson(): JsonObject {
     return JsonObject()
