062c35b6c888e306267d59e1d31daaf7a8e5b8fe
jacadzaca <vitouejj@gmail.com>
2020-04-15 19:21:18 +0200

    Player can now be used over the vertx proxy

--- src/main/kotlin/com/jacadzaca/monopoly/Player.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/Player.kt
@@ -1,3 +1,14 @@
 package com.jacadzaca.monopoly
 
-data class Player(val piece: Piece)
+import io.vertx.codegen.annotations.DataObject
+import io.vertx.core.json.JsonObject
+
+@DataObject
+data class Player(val piece: Piece) {
+  constructor(json: JsonObject) : this(Piece(position = json.getInteger("position")))
+
+  fun toJson(): JsonObject {
+    return JsonObject()
+      .put("position", piece.position)
+  }
+}
