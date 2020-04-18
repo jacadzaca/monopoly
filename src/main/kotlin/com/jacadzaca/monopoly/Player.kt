ef61a97f8f8e62ddd5112d28826c0572676f6fc6
jacadzaca <vitouejj@gmail.com>
2020-04-18 20:52:19 +0200

    redisHashDescription now returns a List

--- src/main/kotlin/com/jacadzaca/monopoly/Player.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/Player.kt
@@ -18,7 +18,7 @@ data class Player(val id: UUID = UUID.randomUUID(), val piece: Piece = Piece()) 
   /**
    * @return a string that can be used to create a hash in Redis
    */
-  fun redisHashDescription(): String {
-    return "player:$id piece:position ${piece.position}"
+  fun redisHashDescription(): List<String> {
+    return "player:$id piece:position ${piece.position}".split(' ')
   }
 }
