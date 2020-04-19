e2bdb5f55b3474731cd1a31c9129452ff4dd9ee1
jacadzaca <vitouejj@gmail.com>
2020-04-19 19:33:33 +0200

    player is no longer a dataclass, removed package.java

--- src/main/kotlin/com/jacadzaca/monopoly/Player.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/Player.kt
@@ -1,20 +1,8 @@
 package com.jacadzaca.monopoly
 
-import io.vertx.codegen.annotations.DataObject
-import io.vertx.core.json.JsonObject
-import java.util.UUID
+import java.util.*
 
-@DataObject
 data class Player(val id: UUID = UUID.randomUUID(), val piece: Piece = Piece()) {
-  constructor(json: JsonObject) : this(
-    UUID.fromString(json.getString("id")),
-    Piece(position = json.getInteger("position")))
-
-  fun toJson(): JsonObject {
-    return JsonObject()
-      .put("position", piece.position)
-  }
-
   /**
    * @return a string that can be used to create a hash in Redis
    */
--- src/main/kotlin/com/jacadzaca/monopoly/package-info.java
+++
@@ -1,4 +0,0 @@
-@ModuleGen(name="monopoly", groupPackage = "com.jacadzaca")
-package com.jacadzaca.monopoly;
-
-import io.vertx.codegen.annotations.ModuleGen;
