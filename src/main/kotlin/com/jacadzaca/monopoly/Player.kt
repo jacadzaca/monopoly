a68e9cf051bdfd0b02429686bc142160196eef05
jacadzaca <vitouejj@gmail.com>
2020-04-19 22:12:18 +0200

    player can now be constructed from a list of Strings

--- src/main/kotlin/com/jacadzaca/monopoly/Player.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/Player.kt
@@ -1,8 +1,12 @@
 package com.jacadzaca.monopoly
 
-import java.util.*
+import java.util.UUID
 
 data class Player(val id: UUID = UUID.randomUUID(), val piece: Piece = Piece()) {
+  companion object {
+    @JvmStatic
+    fun createFromList(list: List<String>): Player = Player(UUID.fromString(list.last()), Piece(list[0].toInt()))
+  }
   /**
    * @return a string that can be used to create a hash in Redis
    */
