80594c43dfcc6a67f5302bf47150930d784025d1
jacadzaca <vitouejj@gmail.com>
2020-04-24 20:41:12 +0200

    server now accepts websocket connections returns hello on a request

---
+++ src/main/kotlin/com/jacadzaca/monopoly/GameAction.kt
@@ -0,0 +1,25 @@
+package com.jacadzaca.monopoly
+
+import io.vertx.core.json.JsonObject
+import io.vertx.kotlin.core.json.Json
+import io.vertx.kotlin.core.json.obj
+import java.util.UUID
+
+data class GameAction(val committerId: UUID, val moveSize: Int) {
+  companion object {
+    @JvmStatic
+    fun isValidJson(json: JsonObject): Boolean {
+      return json.containsKey("committerId") && json.containsKey("moveSize")
+    }
+  }
+
+  constructor(json: JsonObject) : this(
+    UUID.fromString(json.getString("committerId")),
+    json.getInteger("moveSize"))
+
+  fun toJson(): JsonObject {
+    return JsonObject()
+      .put("committerId", committerId)
+      .put("moveSize", moveSize)
+  }
+}
--- src/main/kotlin/com/jacadzaca/monopoly/MainVerticle.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/MainVerticle.kt
@@ -1,12 +1,16 @@
 package com.jacadzaca.monopoly
 
 import io.vertx.core.Promise
+import io.vertx.core.eventbus.DeliveryOptions
 import io.vertx.reactivex.core.AbstractVerticle
+import io.vertx.reactivex.core.RxHelper
+import io.vertx.reactivex.core.buffer.Buffer
 import io.vertx.reactivex.ext.web.Router
 
 class MainVerticle : AbstractVerticle() {
 
   override fun start(startPromise: Promise<Void>) {
+
     val router = Router.router(vertx)
     router.get("/").handler { request ->
       request
@@ -14,8 +18,37 @@ class MainVerticle : AbstractVerticle() {
         .rxSendFile("welcome.html")
         .subscribe()
     }
-    vertx
-      .createHttpServer()
+    val server = vertx.createHttpServer()
+
+    server
+      .webSocketStream()
+      .toFlowable()
+      .doOnNext { connection ->
+        connection
+          .toFlowable()
+          .map(Buffer::toJsonObject)
+          .filter { GameAction.isValidJson(it) }
+          .doOnNext { action ->
+            vertx
+              .eventBus()
+              .rxRequest<String>(connection.path(), action)
+              .map { it.body() }
+              .doOnSuccess { connection.writeTextMessage(it) }
+              .subscribe()
+          }
+          .subscribe()
+      }
+      .doOnError { error ->
+        error.printStackTrace()
+        startPromise.fail(error)
+      }
+      .subscribe()
+
+    RxHelper
+      .deployVerticle(vertx, GameActionsVerticle())
+      .subscribe()
+
+    server
       .requestHandler(router)
       .rxListen(8080)
       .doOnError {error ->
