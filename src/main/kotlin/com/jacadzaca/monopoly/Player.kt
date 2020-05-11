6a36fec6f52818f0caa086f9589b5a4d7800b723
jacadzaca <vitouejj@gmail.com>
2020-05-11 17:09:33 +0200

    Player is now a dataclass

---
+++ src/main/kotlin/com/jacadzaca/monopoly/Player.kt
@@ -0,0 +1,13 @@
+package com.jacadzaca.monopoly
+
+import java.util.*
+
+data class Player(val id: UUID, val piece: Piece) {
+  companion object {
+    fun createFromList(list: List<String>): Player = Player(UUID.fromString(list.last()), Piece(list[0].toInt()))
+  }
+
+  fun redisHashDescription(): List<String> {
+    return "player:$id player:piece:position ${piece.position}".split(' ')
+  }
+}
