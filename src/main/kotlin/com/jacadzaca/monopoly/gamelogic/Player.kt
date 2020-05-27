b0ff86a344cc69e04ae15358dde7da929bdae39e
jacadzaca <vitouejj@gmail.com>
2020-05-27 20:25:01 +0200

    added balance field to Player

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Player.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Player.kt
@@ -1,17 +1,18 @@
 package com.jacadzaca.monopoly.gamelogic
 
-import java.util.*
+import java.math.BigInteger
+import java.util.UUID
 
-data class Player(val id: UUID, val piece: Piece) {
+data class Player(val id: UUID, val piece: Piece, val balance: BigInteger) {
   companion object {
     fun createFromList(list: List<String>): Player =
       Player(
         UUID.fromString(list.last()),
-        Piece(list[0].toInt())
-      )
+        Piece(list[0].toInt()),
+        list[1].toBigInteger())
   }
 
   fun redisHashDescription(): List<String> {
-    return "player:$id player:piece:position ${piece.position}".split(' ')
+    return "player:$id player:piece:position ${piece.position} player:balance $balance".split(' ')
   }
 }
