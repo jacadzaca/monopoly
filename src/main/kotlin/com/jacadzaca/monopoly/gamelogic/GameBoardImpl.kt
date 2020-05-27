3786f9fc422b71588b9b3f773bf4037c60d51713
jacadzaca <vitouejj@gmail.com>
2020-05-27 20:19:36 +0200

    fixed weird formatting

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/GameBoardImpl.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/GameBoardImpl.kt
@@ -1,20 +1,14 @@
 package com.jacadzaca.monopoly.gamelogic
 
 internal class GameBoardImpl(private val boardSize: Int,
-                    private val dieRoller: () -> Int)
+                             private val dieRoller: () -> Int)
   : GameBoard {
   override fun canPlayerExecuteAction(player: Player, action: GameAction): Boolean {
     return player.id == action.committerId
   }
 
   override fun movePlayer(player: Player): Player {
-    return player.copy(piece = Piece(
-      wrapMove(
-        player.piece.position,
-        dieRoller()
-      )
-    )
-    )
+    return player.copy(piece = Piece(wrapMove(player.piece.position, dieRoller())))
   }
 
   private fun wrapMove(currentPosition: Int, move: Int): Int {
