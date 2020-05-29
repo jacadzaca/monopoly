73dbd21619df6386700e7aa309ac799bf014c2f1
jacadzaca <vitouejj@gmail.com>
2020-05-29 21:03:45 +0200

    deleted PlayerMangerRedis

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Player.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Player.kt
@@ -3,16 +3,4 @@ package com.jacadzaca.monopoly.gamelogic
 import java.math.BigInteger
 import java.util.UUID
 
-data class Player(val id: UUID, val piece: Piece, val balance: BigInteger) {
-  companion object {
-    fun createFromList(list: List<String>): Player =
-      Player(
-        UUID.fromString(list.last()),
-        Piece(list[0].toInt()),
-        list[1].toBigInteger())
-  }
-
-  fun redisHashDescription(): List<String> {
-    return "player:$id player:piece:position ${piece.position} player:balance $balance".split(' ')
-  }
-}
+data class Player(val id: UUID, val piece: Piece, val balance: BigInteger)
