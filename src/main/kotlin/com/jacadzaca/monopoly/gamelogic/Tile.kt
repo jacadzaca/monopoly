dc6ea2e389801a8931d5df285f7c9498ec96f92f
jacadzaca <vitouejj@gmail.com>
2020-05-30 18:51:10 +0200

    tiles can change the owner and have prices

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Tile.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Tile.kt
@@ -1,6 +1,9 @@
 package com.jacadzaca.monopoly.gamelogic
 
 import com.jacadzaca.monopoly.gamelogic.buildings.Building
+import java.math.BigInteger
 import java.util.*
 
-data class Tile(val buildings: List<Building>, val owner: UUID?)
+data class Tile(val buildings: List<Building>,
+                val price: BigInteger,
+                var owner: UUID?)
