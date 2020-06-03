15b7421a15165f9f483e13c7c985de991b9018f6
jacadzaca <vitouejj@gmail.com>
2020-06-03 20:26:30 +0200

    BuildingFactory now can tell the price of a certain Building

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/buildings/BuildingFactory.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/buildings/BuildingFactory.kt
@@ -1,5 +1,7 @@
 package com.jacadzaca.monopoly.gamelogic.buildings
 
+import java.math.BigInteger
+
 /**
  * https://en.wikipedia.org/wiki/Flyweight_pattern
  * This class is supposed to manage the reusable building objects
@@ -9,4 +11,5 @@ interface BuildingFactory {
     val standardBuildingFactory: BuildingFactory = StandardBuildingFactory()
   }
   fun create(type: BuildingType): Building
+  fun getPriceFor(type: BuildingType): BigInteger
 }
--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/buildings/StandardBuildingFactory.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/buildings/StandardBuildingFactory.kt
@@ -1,5 +1,7 @@
 package com.jacadzaca.monopoly.gamelogic.buildings
 
+import java.math.BigInteger
+
 internal class StandardBuildingFactory : BuildingFactory {
   private companion object {
     val house = Building(100.toBigInteger())
@@ -10,6 +12,13 @@ internal class StandardBuildingFactory : BuildingFactory {
     return when(type) {
       BuildingType.HOUSE -> house
       BuildingType.HOTEL -> hotel
+    }
+  }
+
+  override fun getPriceFor(type: BuildingType): BigInteger {
+    return when(type) {
+      BuildingType.HOUSE -> 500.toBigInteger()
+      BuildingType.HOTEL -> 2000.toBigInteger()
     }
   }
 }
