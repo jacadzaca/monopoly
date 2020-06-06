7cdb7119cf1f0d416634aa50516208634c532572
jacadzaca <vitouejj@gmail.com>
2020-06-06 19:55:21 +0200

    tiles can count the number of houses on them

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/tiles/Tile.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/tiles/Tile.kt
@@ -1,6 +1,7 @@
 package com.jacadzaca.monopoly.gamelogic.tiles
 
 import com.jacadzaca.monopoly.gamelogic.buildings.Building
+import com.jacadzaca.monopoly.gamelogic.buildings.BuildingType
 import kotlinx.collections.immutable.PersistentList
 import java.math.BigInteger
 import java.util.*
@@ -11,5 +12,9 @@ data class Tile(val buildings: PersistentList<Building>,
 
   fun totalRent(): BigInteger {
     return buildings.map(Building::rent).reduce(BigInteger::add)
+  }
+
+  fun houseCount(): Int {
+    return buildings.filter { it.buildingType == BuildingType.HOUSE }.count()
   }
 }
--- src/test/kotlin/com/jacadzaca/monopoly/gamelogic/tiles/TileTest.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/gamelogic/tiles/TileTest.kt
@@ -5,14 +5,28 @@ import com.jacadzaca.monopoly.createHouse
 import com.jacadzaca.monopoly.createTile
 import kotlinx.collections.immutable.persistentListOf
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-internal class TotalRentCalculatorTest {
+internal class TileTest {
+  private lateinit var tile: Tile
+
+  @BeforeEach
+  fun setUp() {
+    tile = createTile()
+  }
+
   @Test
   fun `getTotalRentFor should sum up the rents of individual buildings`() {
     val rent = 123.toBigInteger()
     val rent1 = 234.toBigInteger()
-    val tile = createTile().copy(buildings = persistentListOf(createHouse(rent), createHotel(rent1)))
+    tile = tile.copy(buildings = persistentListOf(createHouse(rent), createHotel(rent1)))
     assertEquals(rent + rent1,  tile.totalRent())
+  }
+
+  @Test
+  fun `houseCount should count the number of houses on a tile`() {
+    tile = tile.copy(buildings = persistentListOf(createHouse(), createHouse(), createHotel()))
+    assertEquals(2, tile.houseCount())
   }
 }
