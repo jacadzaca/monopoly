eff2aa3a525f8aef2785ee52a31ab5e781f1864a
jacadzaca <vitouejj@gmail.com>
2020-06-17 19:21:45 +0200

    brought back the Player.increasePosition()

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/player/Player.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/player/Player.kt
@@ -8,4 +8,8 @@ data class Player(val id: UUID,
                   val position: Int = 0,
                   val balance: BigInteger,
                   val liability: Liability?) {
+
+  fun increasePosition(by: Int): Player {
+    return copy(position = position + by)
+  }
 }
