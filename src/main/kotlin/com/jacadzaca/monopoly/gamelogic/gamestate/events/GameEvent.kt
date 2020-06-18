02105455be6401c55ddcd046c9fd588155f6cc3e
jacadzaca <vitouejj@gmail.com>
2020-06-18 17:31:37 +0200

    removed GameEvent.apply()

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/GameEvent.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/GameEvent.kt
@@ -1,11 +1,7 @@
 package com.jacadzaca.monopoly.gamelogic.gamestate.events
 
-import com.jacadzaca.monopoly.gamelogic.gamestate.GameState
-import com.jacadzaca.monopoly.gamelogic.gamestate.GameStateManager
 import java.util.*
 
 interface GameEvent {
   val playerId: UUID
-
-  fun apply(gameStateManager: GameStateManager, gameState: GameState): GameState
 }
--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/MoveEvent.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/MoveEvent.kt
@@ -1,12 +1,5 @@
 package com.jacadzaca.monopoly.gamelogic.gamestate.events
 
-import com.jacadzaca.monopoly.gamelogic.gamestate.GameState
-import com.jacadzaca.monopoly.gamelogic.gamestate.GameStateManager
-import com.jacadzaca.monopoly.gamelogic.gamestate.events.GameEvent
 import java.util.*
 
-data class MoveEvent(override val playerId: UUID) :
-  GameEvent {
-  override fun apply(gameStateManager: GameStateManager, gameState: GameState): GameState =
-    gameStateManager.applyEvent(this, gameState)
-}
+data class MoveEvent(override val playerId: UUID) : GameEvent
