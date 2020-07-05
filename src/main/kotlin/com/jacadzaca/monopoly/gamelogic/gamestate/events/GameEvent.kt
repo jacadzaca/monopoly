01445c600b93aa4ca7659ef96351b67f537e00b0
jaca <vitouejj@gmail.com>
2020-07-05 19:57:48 +0200

    add apply() for GameEvent

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/GameEvent.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/GameEvent.kt
@@ -1,7 +1,11 @@
 package com.jacadzaca.monopoly.gamelogic.gamestate.events
 
+import com.jacadzaca.monopoly.gamelogic.gamestate.GameState
+import com.jacadzaca.monopoly.gamelogic.gamestate.GameStateManager
 import com.jacadzaca.monopoly.gamelogic.player.PlayerID
 
 interface GameEvent {
   val playerId: PlayerID
+
+  fun apply(gameStateManager: GameStateManager, gameState: GameState): GameState
 }
