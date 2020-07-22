2cc9b7e3c7d04839edc11f9338f08f2952e9c870
jaca <vitouejj@gmail.com>
2020-07-22 13:18:56 +0200

    GameEvent Verifier now uses VerificationResult instead of VerifiedGameEvent

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/GameEventVerifier.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/GameEventVerifier.kt
@@ -2,6 +2,12 @@ package com.jacadzaca.monopoly.gamelogic.gamestate.events
 
 import com.jacadzaca.monopoly.gamelogic.gamestate.GameState
 
-interface GameEventVerifier<T : GameEvent, V : VerifiedGameEvent> {
-  fun verify(event: T, gameState: GameState): V?
+interface GameEventVerifier<T : GameEvent> {
+  companion object {
+    internal const val invalidPlayerId = "No player with specified ID exist"
+    internal const val invalidTileIndex = "There exist no tile at specified tile index"
+    internal const val buyerHasInsufficientBalance = "Buyer dose not have enough funds to preform requested action"
+  }
+
+  fun verify(event: T, gameState: GameState): VerificationResult?
 }
