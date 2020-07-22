222229c60cda70304e3305c4b22f6668f82eec8f
jaca <vitouejj@gmail.com>
2020-07-22 13:19:17 +0200

    EstatePurchase verifier refactor

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/estatepurchase/EstatePurchaseEventVerifier.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/estatepurchase/EstatePurchaseEventVerifier.kt
@@ -4,24 +4,31 @@ import com.jacadzaca.monopoly.gamelogic.estates.EstateFactory
 import com.jacadzaca.monopoly.gamelogic.estates.EstateType
 import com.jacadzaca.monopoly.gamelogic.gamestate.GameState
 import com.jacadzaca.monopoly.gamelogic.gamestate.events.GameEventVerifier
-import com.jacadzaca.monopoly.gamelogic.player.PlayerID
+import com.jacadzaca.monopoly.gamelogic.gamestate.events.VerificationResult
 
 internal class EstatePurchaseEventVerifier(
   private val estateFactory: EstateFactory,
   private val requiredHousesForHotel: Int,
   private val tileExists: (Int, GameState) -> Boolean
-) : GameEventVerifier<EstatePurchaseEvent, VerifiedEstatePurchaseEvent> {
-  override fun verify(event: EstatePurchaseEvent, gameState: GameState): VerifiedEstatePurchaseEvent? {
-    val buyer = gameState.players[event.playerId]
-    if (!tileExists(event.tileIndex, gameState) || buyer == null) {
-      return null
+) : GameEventVerifier<EstatePurchaseEvent> {
+  companion object {
+    internal const val tileNotOwnedByBuyer = "Buyer dose not own the tile where he wants to buy a estate"
+    internal const val notEnoughHouses = "There are not enough houses on the tile where a hotel is to be placed "
+  }
+
+  override fun verify(event: EstatePurchaseEvent, gameState: GameState): VerificationResult {
+    val buyer = gameState.players[event.playerId] ?: return VerificationResult.Failure(GameEventVerifier.invalidPlayerId)
+    if (!tileExists(event.tileIndex, gameState)) {
+      return VerificationResult.Failure(GameEventVerifier.invalidTileIndex)
     }
     val tile = gameState.tiles[event.tileIndex]
     return when {
-      tile.owner != buyer.id -> null
-      estateFactory.getPriceFor(event.estateType) > buyer.balance -> null
-      event.estateType == EstateType.HOTEL && tile.houseCount() < requiredHousesForHotel -> null
-      else -> VerifiedEstatePurchaseEvent(event)
+      tile.owner != buyer.id -> VerificationResult.Failure(tileNotOwnedByBuyer)
+      estateFactory.getPriceFor(event.estateType) > buyer.balance -> VerificationResult.Failure(GameEventVerifier.buyerHasInsufficientBalance)
+      event.estateType == EstateType.HOTEL && tile.houseCount() < requiredHousesForHotel -> VerificationResult.Failure(
+        notEnoughHouses
+      )
+      else -> VerificationResult.VerifiedEstatePurchaseEvent(buyer, event.estateType, tile)
     }
   }
 }
--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/tilepurchase/TilePurchaseEventVerifier.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/tilepurchase/TilePurchaseEventVerifier.kt
@@ -2,21 +2,28 @@ package com.jacadzaca.monopoly.gamelogic.gamestate.events.tilepurchase
 
 import com.jacadzaca.monopoly.gamelogic.gamestate.GameState
 import com.jacadzaca.monopoly.gamelogic.gamestate.events.GameEventVerifier
-import com.jacadzaca.monopoly.gamelogic.player.PlayerID
+import com.jacadzaca.monopoly.gamelogic.gamestate.events.GameEventVerifier.Companion.buyerHasInsufficientBalance
+import com.jacadzaca.monopoly.gamelogic.gamestate.events.GameEventVerifier.Companion.invalidPlayerId
+import com.jacadzaca.monopoly.gamelogic.gamestate.events.GameEventVerifier.Companion.invalidTileIndex
+import com.jacadzaca.monopoly.gamelogic.gamestate.events.VerificationResult
 
 internal class TilePurchaseEventVerifier(
   private val tileExists: (Int, GameState) -> Boolean
-) : GameEventVerifier<TilePurchaseEvent, VerifiedTilePurchaseEvent> {
-  override fun verify(event: TilePurchaseEvent, gameState: GameState): VerifiedTilePurchaseEvent? {
-    val buyer = gameState.players[event.playerId]
-    if (!tileExists(event.tileIndex, gameState) || buyer == null) {
-      return null
+) : GameEventVerifier<TilePurchaseEvent> {
+  companion object {
+    internal const val tileAlreadyHasOwner = "Tile that the player wants to buy already has an owner "
+  }
+
+  override fun verify(event: TilePurchaseEvent, gameState: GameState): VerificationResult {
+    val buyer = gameState.players[event.playerId] ?: return VerificationResult.Failure(invalidPlayerId)
+    if (!tileExists(event.tileIndex, gameState)) {
+      return VerificationResult.Failure(invalidTileIndex)
     }
     val tile = gameState.tiles[event.tileIndex]
     return when {
-      tile.owner != null -> null
-      tile.price > buyer.balance -> null
-      else -> VerifiedTilePurchaseEvent(event)
+      tile.owner != null -> VerificationResult.Failure(tileAlreadyHasOwner)
+      tile.price > buyer.balance -> VerificationResult.Failure(buyerHasInsufficientBalance)
+      else -> VerificationResult.VerifiedTilePurchaseEvent(buyer, tile)
     }
   }
 }
