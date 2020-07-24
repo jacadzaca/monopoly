9942f864b8d80b7a8800358e5ddc96cbfb6b578f
jaca <vitouejj@gmail.com>
2020-07-24 19:46:32 +0200

    fix MoveEventApplierTest

--- src/test/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/move/MoveEventApplierTest.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/move/MoveEventApplierTest.kt
@@ -2,34 +2,39 @@ package com.jacadzaca.monopoly.gamelogic.gamestate.events.move
 
 import com.jacadzaca.monopoly.gamelogic.gamestate.GameState
 import com.jacadzaca.monopoly.gamelogic.gamestate.events.VerificationResult
+import com.jacadzaca.monopoly.gamelogic.player.Player
 import com.jacadzaca.monopoly.gamelogic.player.PlayerMover
 import com.jacadzaca.monopoly.getTestPlayer
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import kotlin.random.Random
 
 internal class MoveEventApplierTest {
-  private val boardSize = 20
   private val player = getTestPlayer()
   private val gameState = mockk<GameState>()
-  private val updateGameState = mockk<GameState>()
   private val playerMover = mockk<PlayerMover>()
   private val eventApplier = MoveEventApplier(playerMover)
   private val event = VerificationResult.VerifiedMoveEvent(player, player.id)
 
   @BeforeEach
   fun setUp() {
-    every { gameState.boardSize } returns boardSize
-    every { gameState.update(event.playerId, any()) } returns updateGameState
+    val playerSlot = slot<Player>()
+    every { gameState.boardSize } returns 20
+    every { gameState.update(event.playerId, capture(playerSlot)) } answers {
+      every { gameState.players[event.playerId] } returns playerSlot.captured
+      gameState
+    }
   }
 
   @Test
   fun `apply should update the mover's position`() {
-    val movedPlayer = player.copy(position = 10)
-    every { playerMover.move(event.player, boardSize) } returns movedPlayer
-    eventApplier.apply(event, gameState)
-    assertEquals(updateGameState, gameState.update(event.playerId, movedPlayer))
+    val movedPlayer = player.copy(position = Random.nextInt())
+    every { playerMover.move(event.player, gameState.boardSize) } returns movedPlayer
+    val actual = eventApplier.apply(event, gameState)
+    assertEquals(movedPlayer.position, actual.players[event.playerId]!!.position)
   }
 }
