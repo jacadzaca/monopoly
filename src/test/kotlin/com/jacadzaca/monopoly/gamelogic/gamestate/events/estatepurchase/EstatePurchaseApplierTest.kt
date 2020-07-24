64b2f05d14d55474ea7fee7b1bc47d28d44d5973
jaca <vitouejj@gmail.com>
2020-07-24 20:05:33 +0200

    refactor EsatePurchaseApplierTest

--- src/test/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/estatepurchase/EstatePurchaseApplierTest.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/estatepurchase/EstatePurchaseApplierTest.kt
@@ -19,36 +19,38 @@ import org.junit.jupiter.api.Test
 internal class EstatePurchaseApplierTest {
   private val tile = createTile()
   private val buyer = getTestPlayer()
-  private val tileSlot = slot<Tile>()
-  private val playerSlot = slot<Player>()
   private val gameState = mockk<GameState>()
   private val estateFactory = mockk<EstateFactory>()
-  private val updatedGameState =  mockk<GameState>()
   private val eventApplier = EstatePurchaseApplier(estateFactory)
   private val event = VerificationResult.VerifiedEstatePurchaseEvent(buyer, buyer.id, tile, 0, EstateType.HOUSE)
 
   @BeforeEach
   fun setUp() {
+    val tileSlot = slot<Tile>()
+    val playerSlot = slot<Player>()
     every { estateFactory.getPriceFor(event.estateType) } returns buyer.balance - 10.toBigInteger()
     every { estateFactory.create(event.estateType) } returns createHouse()
-    // WARNING! this makes the order of updates a tested condition - first update the tile then the player
-    every { gameState.update(event.tileIndex, capture(tileSlot)) } returns updatedGameState
-    every { updatedGameState.update(event.playerId, capture(playerSlot)) } returns updatedGameState
+    every { gameState.update(event.tileIndex, capture(tileSlot)) } answers {
+      every { gameState.tiles[event.tileIndex] } returns tileSlot.captured
+      gameState
+    }
+    every { gameState.update(event.playerId, capture(playerSlot)) } answers {
+      every { gameState.players[event.playerId] } returns playerSlot.captured
+      gameState
+    }
   }
 
   @Test
-  fun `apply should add a estate to the tile`() {
+  fun `apply adds a estate to the tile`() {
     val tileWithEstate = tile.copy(estates = tile.estates.add(estateFactory.create(event.estateType)))
     val actual = eventApplier.apply(event, gameState)
-    every { updatedGameState.tiles[event.tileIndex] } returns tileSlot.captured
     assertEquals(tileWithEstate.estates, actual.tiles[event.tileIndex].estates)
   }
 
   @Test
-  fun `apply should detract from the buyer's balance`() {
+  fun `apply detracts from the buyer's balance`() {
     val playerWithDetractedFunds = buyer.copy(balance = buyer.balance - estateFactory.getPriceFor(event.estateType))
     val actual = eventApplier.apply(event, gameState)
-    every { updatedGameState.players[event.playerId] } returns playerSlot.captured
     assertEquals(playerWithDetractedFunds.balance, actual.players[buyer.id]!!.balance)
   }
 }
