ef681a87f0ee5ba7cc4844b3829bf8cffb886a87
jaca <vitouejj@gmail.com>
2020-07-26 23:57:02 +0200

    remove PlayerMover

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/player/DiceRollPlayerMover.kt
+++
@@ -1,7 +0,0 @@
-package com.jacadzaca.monopoly.gamelogic.player
-
-internal class DiceRollPlayerMover(private val dieRoller: () -> (Int)) : PlayerMover {
-  override fun move(toMove: Player, boardSize: Int): Player {
-    return toMove.copy(position = (toMove.position + dieRoller()) % boardSize)
-  }
-}
--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/player/PlayerMover.kt
+++
@@ -1,5 +0,0 @@
-package com.jacadzaca.monopoly.gamelogic.player
-
-interface PlayerMover {
-  fun move(toMove: Player, boardSize: Int): Player
-}
