d1ef5aca3625d7fd8330284d3bc2c90963b1c5c7
jaca <vitouejj@gmail.com>
2020-07-26 23:04:54 +0200

    Revert "Revert "Revert "move PlayerPaysLiabilityEvent to GameEvent sealed class"""
    This reverts commit ef97759b1de773f326b4ab7bafc93a53ecd868de.

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/GameEvent.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/GameEvent.kt
@@ -1,11 +1,7 @@
 package com.jacadzaca.monopoly.gamelogic.gamestate.events
 
-import com.jacadzaca.monopoly.gamelogic.player.Liability
-import com.jacadzaca.monopoly.gamelogic.player.Player
 import com.jacadzaca.monopoly.gamelogic.player.PlayerID
 
 sealed class GameEvent {
-  data class MoveEvent(val moverId: PlayerID)
-  data class TilePurchaseEvent(val buyerId: PlayerID, val tileIndex: Int)
-  data class PlayerPaysLiabilityEvent(val payer: Player, val payerId: PlayerID, val liability: Liability)
+  data class MoveEvent(val moverId: PlayerID) : GameEvent()
 }
