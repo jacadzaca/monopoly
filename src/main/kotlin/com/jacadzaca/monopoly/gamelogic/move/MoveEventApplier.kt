ef8f1ffac328b883af5ba9bc0eff9899b794db13
jaca <vitouejj@gmail.com>
2020-07-26 23:57:19 +0200

    move PlayerMover responsibilites to MoveEventApplier

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/move/MoveEventApplier.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/move/MoveEventApplier.kt
@@ -3,12 +3,11 @@ package com.jacadzaca.monopoly.gamelogic.move
 import com.jacadzaca.monopoly.gamelogic.GameState
 import com.jacadzaca.monopoly.gamelogic.GameEventApplier
 import com.jacadzaca.monopoly.gamelogic.VerificationResult.VerifiedMoveEvent
-import com.jacadzaca.monopoly.gamelogic.player.PlayerMover
 
-internal class MoveEventApplier(private val playerMover: PlayerMover) :
+internal class MoveEventApplier(private val rollDice: () -> Int) :
   GameEventApplier<VerifiedMoveEvent> {
   override fun apply(event: VerifiedMoveEvent, gameState: GameState): GameState {
     return gameState
-      .update(event.moverId, playerMover.move(event.mover, gameState.boardSize))
+      .update(event.moverId, event.mover.copy(position = (event.mover.position + rollDice()) % gameState.boardSize))
   }
 }
--- src/test/kotlin/com/jacadzaca/monopoly/gamelogic/move/MoveEventApplierTest.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/gamelogic/move/MoveEventApplierTest.kt
@@ -3,8 +3,6 @@ package com.jacadzaca.monopoly.gamelogic.move
 import com.jacadzaca.monopoly.gamelogic.GameState
 import com.jacadzaca.monopoly.gamelogic.VerificationResult
 import com.jacadzaca.monopoly.gamelogic.player.Player
-import com.jacadzaca.monopoly.gamelogic.player.PlayerMover
-import com.jacadzaca.monopoly.createPlayer
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.slot
@@ -15,28 +13,45 @@ import java.util.*
 import kotlin.random.Random
 
 internal class MoveEventApplierTest {
-  private val player = createPlayer()
+  private val player = mockk<Player>()
   private val gameState = mockk<GameState>()
-  private val playerMover = mockk<PlayerMover>()
-  private val eventApplier = MoveEventApplier(playerMover)
+  private val rollDice = mockk<() -> Int>()
+  private val eventApplier = MoveEventApplier(rollDice)
   private val event = VerificationResult.VerifiedMoveEvent(player, UUID.randomUUID())
 
   @BeforeEach
   fun setUp() {
-    val playerSlot = slot<Player>()
+    val calculatedPositionSlot = slot<Int>()
     every { gameState.boardSize } returns 20
-    every { gameState.update(event.moverId, capture(playerSlot)) } answers {
-      every { gameState.players[event.moverId] } returns playerSlot.captured
-      gameState
+    every { gameState.update(event.moverId, any()) } returns gameState
+    every { gameState.players[event.moverId] } returns player
+    every { rollDice() } returns Random.nextInt(1, gameState.boardSize - 1)
+    every { player.position } returns gameState.boardSize - rollDice() - 1
+    every { player.copy(position = capture(calculatedPositionSlot)) } answers {
+      every { player.position } returns calculatedPositionSlot.captured
+      player
     }
   }
 
   @Test
-  fun `apply updates the mover's position`() {
-    val movedPlayer = player.copy(position = Random.nextInt())
-    val boardSize = gameState.boardSize
-    every { playerMover.move(event.mover, boardSize) } returns movedPlayer
+  fun `apply add dieRoller to the player's position`() {
+    val previousPosition = player.position
+    val expected = previousPosition + rollDice()
     val actual = eventApplier.apply(event, gameState)
-    assertEquals(movedPlayer.position, actual.players[event.moverId]!!.position)
+    assertEquals(
+      expected,
+      actual.players[event.moverId]!!.position
+    )
+  }
+
+  @Test
+  fun `apply wraps position calculation`() {
+    every { player.position } returns gameState.boardSize - 1
+    val expected = rollDice() - 1
+    val actual = eventApplier.apply(event, gameState)
+    assertEquals(
+      expected,
+      actual.players[event.moverId]!!.position
+    )
   }
 }
