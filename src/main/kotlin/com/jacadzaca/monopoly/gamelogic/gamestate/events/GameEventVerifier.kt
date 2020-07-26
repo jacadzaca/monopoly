76f27df061d889db8603803378d937a15887c964
jaca <vitouejj@gmail.com>
2020-07-26 23:07:18 +0200

    Revert "GameEventVerifier now has static instances of each verifier type"
    This reverts commit 5f851d5b57a1359b40f65792406e7d0ec4e0348e.

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/GameEventVerifier.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/gamestate/events/GameEventVerifier.kt
@@ -1,19 +1,9 @@
 package com.jacadzaca.monopoly.gamelogic.gamestate.events
 
-import com.jacadzaca.monopoly.gamelogic.estates.EstateFactory
 import com.jacadzaca.monopoly.gamelogic.gamestate.GameState
-import com.jacadzaca.monopoly.gamelogic.gamestate.events.estatepurchase.EstatePurchaseEventVerifier
-import com.jacadzaca.monopoly.gamelogic.gamestate.events.move.MoveEventVerifier
-import com.jacadzaca.monopoly.gamelogic.gamestate.events.tilepurchase.TilePurchaseEventVerifier
 
 interface GameEventVerifier<in T> {
   companion object {
-    val standardMoveEventVerifier: GameEventVerifier<GameEvent.MoveEvent> = MoveEventVerifier()
-    val standardTilePurchaseEventVerifier: GameEventVerifier<GameEvent.TilePurchaseEvent> =
-      TilePurchaseEventVerifier(::tileExists)
-    val standardEstatePurchaseEventVerifier: GameEventVerifier<GameEvent.EstatePurchaseEvent> = EstatePurchaseEventVerifier(
-      EstateFactory.standardEstateFactory, 4, ::tileExists
-    )
     internal const val invalidPlayerId = "No player with specified ID exist"
     internal const val invalidTileIndex = "There exist no tile at specified tile index"
     internal const val buyerHasInsufficientBalance = "Buyer dose not have enough funds to preform requested action"
