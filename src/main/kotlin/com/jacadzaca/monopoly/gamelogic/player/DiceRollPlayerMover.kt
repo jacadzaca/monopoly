946735a946293083b1bd0ea31cb060732a4a6df5
jaca <vitouejj@gmail.com>
2020-07-26 23:30:03 +0200

    refactor DiceRollPlayerMover

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/player/DiceRollPlayerMover.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/player/DiceRollPlayerMover.kt
@@ -2,10 +2,6 @@ package com.jacadzaca.monopoly.gamelogic.player
 
 internal class DiceRollPlayerMover(private val dieRoller: () -> (Int)) : PlayerMover {
   override fun move(toMove: Player, boardSize: Int): Player {
-    return toMove.copy(position = wrap(toMove.position + dieRoller(), boardSize))
-  }
-
-  private fun wrap(toWrap: Int, max: Int): Int {
-    return toWrap % max
+    return toMove.copy(position = (toMove.position + dieRoller()) % boardSize)
   }
 }
