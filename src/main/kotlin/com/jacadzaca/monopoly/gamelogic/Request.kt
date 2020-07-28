639e1a9d87e399baa67c6043c6c34cf499aabbcd
jaca <vitouejj@gmail.com>
2020-07-28 18:17:05 +0200

    add Request

---
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Request.kt
@@ -0,0 +1,9 @@
+package com.jacadzaca.monopoly.gamelogic
+
+interface Request {
+  companion object {
+    internal const val invalidPlayerId = "No player with specified ID exist"
+    internal const val buyerHasInsufficientBalance = "Buyer dose not have enough funds to preform requested action"
+  }
+  fun validate(context: GameState): ValidationResult
+}
