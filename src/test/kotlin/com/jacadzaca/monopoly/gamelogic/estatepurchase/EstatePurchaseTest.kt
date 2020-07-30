3061d47e6669436ed07cbf2ac153bde8c3001030
jaca <vitouejj@gmail.com>
2020-07-30 17:58:48 +0200

    refactor EstatPurchaseTest

--- src/test/kotlin/com/jacadzaca/monopoly/gamelogic/estatepurchase/EstatePurchaseTest.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/gamelogic/estatepurchase/EstatePurchaseTest.kt
@@ -5,61 +5,59 @@ import com.jacadzaca.monopoly.gamelogic.Player
 import com.jacadzaca.monopoly.gamelogic.Tile
 import com.jacadzaca.monopoly.gamelogic.estates.Estate
 import com.jacadzaca.monopoly.gamelogic.estates.EstateType
+import io.mockk.clearMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 import java.math.BigInteger
 import java.util.*
 import kotlin.random.Random
 
 internal class EstatePurchaseTest {
-  private val tile = mockk<Tile>()
-  private val buyer = mockk<Player>()
   private val tileIndex = Random.nextInt()
   private val buyersId = UUID.randomUUID()
   private val gameState = mockk<GameState>()
+  private val tile = mockk<Tile>(relaxed = true)
+  private val buyer = mockk<Player>(relaxed = true)
   private val create = mockk<(EstateType) -> Estate>()
-  private val housePrice = Random.nextInt().toBigInteger()
-  private val hotelPrice = Random.nextInt().toBigInteger()
   private val priceOf = mockk<(EstateType) -> BigInteger>()
-  private val housePurchase = EstatePurchase(buyer, buyersId, tile, tileIndex, EstateType.HOUSE, create, priceOf)
-  private val hotelPurchase = EstatePurchase(buyer, buyersId, tile, tileIndex, EstateType.HOTEL, create, priceOf)
 
   @BeforeEach
   fun setUp() {
-    val house = mockk<Estate>(name = "house")
-    val hotel = mockk<Estate>(name = "hotel")
-    every { create(EstateType.HOUSE) } returns house
-    every { create(EstateType.HOTEL) } returns hotel
-    every { priceOf(EstateType.HOUSE) } returns housePrice
-    every { priceOf(EstateType.HOTEL) } returns hotelPrice
+    clearMocks(gameState, tile, buyer, create, priceOf)
+    every { priceOf(any()) } returns mockk()
+    every { create(any()) } returns mockk()
     every { gameState.update(buyersId, any()) } returns gameState
     every { gameState.update(tileIndex, any()) } returns gameState
-    every { tile.addEstate(house) } returns mockk(name = "tile with added house")
-    every { tile.addEstate(hotel) } returns mockk(name = "tile with added hotel")
-    every { buyer.detractFunds(housePrice) } returns mockk(name = "player after detracting a price of a house")
-    every { buyer.detractFunds(hotelPrice) } returns mockk(name = "player after detracting a price of a hotel")
   }
 
-  @Test
-  fun `apply adds a estate to the tile`() {
-    housePurchase.apply(gameState)
-    val tileWithAddedHouse = tile.addEstate(create(EstateType.HOUSE))
+  @ParameterizedTest
+  @EnumSource(EstateType::class)
+  fun `apply adds a estate to the tile`(estateType: EstateType) {
+    val estate = mockk<Estate>(name = "estate")
+    val tileWithAddedHouse = mockk<Tile>(name = "tile with added estate")
+    every { tile.addEstate(estate) } returns tileWithAddedHouse
+    every { create(estateType) } returns estate
+    purchaseOfType(estateType).apply(gameState)
     verify { gameState.update(tileIndex, tileWithAddedHouse) }
-    hotelPurchase.apply(gameState)
-    val tileWithAddedHotel = tile.addEstate(create(EstateType.HOTEL))
-    verify { gameState.update(tileIndex, tileWithAddedHotel) }
   }
 
-  @Test
-  fun `apply detracts from the buyer's balance`() {
-    housePurchase.apply(gameState)
-    val playerAfterBuyingHouse = buyer.detractFunds(housePrice)
-    verify { gameState.update(buyersId, playerAfterBuyingHouse) }
-    hotelPurchase.apply(gameState)
-    val playerAfterBuyingHotel = buyer.detractFunds(hotelPrice)
-    verify { gameState.update(buyersId, playerAfterBuyingHotel) }
+  @ParameterizedTest
+  @EnumSource(EstateType::class)
+  fun `apply detracts from the buyer's balance`(estateType: EstateType) {
+    val estatePrice = Random.nextInt().toBigInteger()
+    val playerAfterPurchase = mockk<Player>(name = "playerAfterPurchase")
+    every { priceOf(estateType) } returns estatePrice
+    every { buyer.detractFunds(estatePrice) } returns playerAfterPurchase
+    purchaseOfType(estateType).apply(gameState)
+    verify { gameState.update(buyersId, playerAfterPurchase) }
   }
+
+  private fun purchaseOfType(estateType: EstateType): EstatePurchase =
+    EstatePurchase(buyer, buyersId, tile, tileIndex, estateType, create, priceOf)
 }
