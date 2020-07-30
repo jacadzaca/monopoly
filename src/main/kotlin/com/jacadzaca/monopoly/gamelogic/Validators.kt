382ea914bf4758f6cb2db338686b396c6f140962
jaca <vitouejj@gmail.com>
2020-07-30 18:12:39 +0200

    remove tileExists() functio

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Validators.kt
+++
@@ -1,5 +0,0 @@
-package com.jacadzaca.monopoly.gamelogic
-
-import com.jacadzaca.monopoly.gamelogic.GameState
-
-fun tileExists(tileIndex: Int, gameState: GameState): Boolean = gameState.boardSize > tileIndex && tileIndex > 0
