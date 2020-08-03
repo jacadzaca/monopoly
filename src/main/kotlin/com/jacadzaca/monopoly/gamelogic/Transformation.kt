acb541d144ab912b8ff9ff0b965afb84e68d27fa
jaca <vitouejj@gmail.com>
2020-08-03 18:55:48 +0200

    add PlayerMovement Transformation

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Transformation.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Transformation.kt
@@ -1,4 +1,7 @@
 package com.jacadzaca.monopoly.gamelogic
 
+import java.util.*
+
 sealed class Transformation {
+  data class PlayerMovement(val movedPlayerId: UUID, val movedBy: Int) : Transformation()
 }
