1e5b02d47c62abe4c84fa5019e819be02268f209
jaca <vitouejj@gmail.com>
2020-08-05 18:44:24 +0200

    Revert "PlayerMoves now adds a transformation to updated GameState"
    This reverts commit e1005f239db3703ee910e6908f020c8847a37ab6.

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/moving/PlayerMoves.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/moving/PlayerMoves.kt
@@ -3,7 +3,6 @@ package com.jacadzaca.monopoly.gamelogic.moving
 import com.jacadzaca.monopoly.gamelogic.GameState
 import com.jacadzaca.monopoly.gamelogic.Player
 import com.jacadzaca.monopoly.gamelogic.Action
-import com.jacadzaca.monopoly.gamelogic.Transformation
 import java.util.*
 import kotlin.random.Random
 
@@ -13,9 +12,7 @@ data class PlayerMoves(
   private val rollDice: () -> Int
 ) : Action {
   override fun apply(target: GameState): GameState {
-    val diceRoll = rollDice()
     return target
-      .update(playersId, player.copy(position = (player.position + diceRoll) % target.boardSize))
-      .addTransformation(Transformation.PlayerMovement(playersId, diceRoll))
+      .update(playersId, player.copy(position = (player.position + rollDice()) % target.boardSize))
   }
 }
