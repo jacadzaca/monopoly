270020d213d3e1048f7776b293cf292b6fc9449f
jaca <vitouejj@gmail.com>
2020-08-09 16:54:13 +0200

    refactor TileTest to use mocks

--- src/test/kotlin/com/jacadzaca/monopoly/gamelogic/TileTest.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/gamelogic/TileTest.kt
@@ -1,53 +1,54 @@
 package com.jacadzaca.monopoly.gamelogic
 
 import com.jacadzaca.monopoly.gamelogic.estates.Estate
-import io.mockk.spyk
-import io.mockk.verify
+import io.mockk.*
 import kotlinx.collections.immutable.persistentListOf
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.random.Random
 
 internal class TileTest {
   private val houses = spyk(persistentListOf<Estate>())
   private val hotels = spyk(persistentListOf<Estate>())
+  private val house = mockk<Estate.House>(name = "house")
+  private val hotel = mockk<Estate.Hotel>(name = "hotel")
   private val tile = Tile(houses, hotels, Random.nextInt().toBigInteger(), null)
+
+  @BeforeEach
+  fun setUp() {
+    clearAllMocks()
+    every { house.rent } returns Random.nextInt().toBigInteger()
+    every { hotel.rent } returns Random.nextInt().toBigInteger()
+  }
 
   @Test
   fun `totalRent consists of rent for hotels and houses`() {
-    val house = Estate.House(Random.nextInt().toBigInteger())
-    val hotel = Estate.Hotel(Random.nextInt().toBigInteger())
     val tile = tile.copy(houses = houses.add(house), hotels = hotels.add(hotel))
     assertEquals(house.rent + hotel.rent, tile.totalRent())
   }
 
   @Test
   fun `totalRent sums up rent from all houses`() {
-    val house = Estate.House(Random.nextInt().toBigInteger())
-    val house1 = Estate.House(Random.nextInt().toBigInteger())
-    val tile = tile.copy(houses = houses.addAll(listOf(house, house1)))
-    assertEquals(house.rent + house1.rent, tile.totalRent())
+    val tile = tile.copy(houses = houses.addAll(listOf(house, house)))
+    assertEquals(house.rent + house.rent, tile.totalRent())
   }
 
   @Test
   fun `totalRent sums up rent from all hotels`() {
-    val hotel = Estate.Hotel(Random.nextInt().toBigInteger())
-    val hotel1 = Estate.Hotel(Random.nextInt().toBigInteger())
-    val tile = tile.copy(hotels = hotels.addAll(listOf(hotel, hotel1)))
-    assertEquals(hotel.rent + hotel1.rent, tile.totalRent())
+    val tile = tile.copy(hotels = hotels.addAll(listOf(hotel, hotel)))
+    assertEquals(hotel.rent + hotel.rent, tile.totalRent())
   }
 
   @Test
   fun `addEstate adds the estate to houses if it's a house`() {
-    val house = Estate.House(Random.nextInt().toBigInteger())
     assertTrue(tile.addEstate(house).houses.contains(house))
     verify { houses.add(house) }
   }
 
   @Test
   fun `addEstate adds the estate to hotels it's a hotel`() {
-    val hotel = Estate.Hotel(Random.nextInt().toBigInteger())
     assertTrue(tile.addEstate(hotel).hotels.contains(hotel))
     verify { hotels.add(hotel) }
   }
