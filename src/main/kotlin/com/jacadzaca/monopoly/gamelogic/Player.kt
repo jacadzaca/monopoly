f6e1265afdded005305e4950bf307d3ec52365fb
jaca <vitouejj@gmail.com>
2020-08-11 17:00:29 +0200

    add Player.setPosition method

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Player.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Player.kt
@@ -6,6 +6,7 @@ data class Player(
   val position: Int = 0,
   val balance: BigInteger
 ) {
+  fun setPosition(newPosition: Int): Player = copy(position = newPosition)
   fun addFunds(toAdd: BigInteger): Player = copy(balance = balance + toAdd)
   fun detractFunds(toDetract: BigInteger): Player = copy(balance = balance - toDetract)
 }
