c7f11cf1301b1155e3e3cf040429b97466a97808
jaca <vitouejj@gmail.com>
2020-08-11 20:59:02 +0200

    reformat PlayerMovementRequesstTest

--- src/test/kotlin/com/jacadzaca/monopoly/requests/PlayerMovementRequestTest.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/requests/PlayerMovementRequestTest.kt
@@ -13,26 +13,19 @@ internal class PlayerMovementRequestTest {
   private val playersId = UUID.randomUUID()
   private val gameState = mockk<GameState>()
   private val actionCreator = mockk<(Player, UUID, GameState) -> PlayerMoves>()
-  private val request =
-    PlayerMovementRequest(playersId, actionCreator, gameState)
+  private val request = PlayerMovementRequest(playersId, actionCreator, gameState)
 
   @Test
   fun `validate returns verifiedEvent if the player with given id exists`() {
     every { gameState.players[playersId] } returns player
     val createdAction = mockk<PlayerMoves>(name = "created PlayerMoves")
     every { actionCreator(player, playersId, gameState) } returns createdAction
-    assertEquals(
-      ValidationResult.Success(createdAction),
-      request.validate()
-    )
+    assertEquals(ValidationResult.Success(createdAction), request.validate())
   }
 
   @Test
   fun `validate returns Failure if the event references a non-existing player`() {
     every { gameState.players[playersId] } returns null
-    assertEquals(
-      ValidationResult.Failure(Request.invalidPlayerId),
-      request.validate()
-    )
+    assertEquals(ValidationResult.Failure(Request.invalidPlayerId), request.validate())
   }
 }
