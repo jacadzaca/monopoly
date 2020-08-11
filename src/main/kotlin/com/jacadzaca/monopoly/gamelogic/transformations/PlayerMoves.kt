fe05c028da0cd391e8f53a2316171c5b588101df
jaca <vitouejj@gmail.com>
2020-08-11 17:33:20 +0200

    PlayerMoves now makes player pay a liability if he steps on a tile owned by different player

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/transformations/PlayerMoves.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/transformations/PlayerMoves.kt
@@ -1,18 +1,27 @@
 package com.jacadzaca.monopoly.gamelogic.transformations
 
 import com.jacadzaca.monopoly.gamelogic.GameState
+import com.jacadzaca.monopoly.gamelogic.Liability
 import com.jacadzaca.monopoly.gamelogic.Player
-import java.text.FieldPosition
 import java.util.*
 
+/**
+ * target.boardSize > PlayerMoves.newPosition >= 0
+ */
 data class PlayerMoves(
   private val player: Player,
   private val playersId: UUID,
   val newPosition: Int,
-  private val target: GameState
+  private val target: GameState,
+  private val createPayment: (Player, UUID, Liability, GameState) -> (LiabilityPayment)
 ) : Transformation {
   override fun transform(): GameState {
-    return target
-      .update(playersId, player.setPosition(newPosition))
+    val tile = target.tiles[newPosition]
+    return if (tile.ownersId != null && tile.ownersId != playersId) {
+      val liability = Liability(tile.totalRent(), target.players[tile.ownersId]!!, tile.ownersId)
+      createPayment(player, playersId, liability, target).transform()
+    } else {
+      target
+    }.update(playersId, player.setPosition(newPosition))
   }
 }
--- src/test/kotlin/com/jacadzaca/monopoly/gamelogic/transformations/PlayerMovesTest.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/gamelogic/transformations/PlayerMovesTest.kt
@@ -1,12 +1,12 @@
 package com.jacadzaca.monopoly.gamelogic.transformations
 
 import com.jacadzaca.monopoly.gamelogic.GameState
+import com.jacadzaca.monopoly.gamelogic.Liability
 import com.jacadzaca.monopoly.gamelogic.Player
+import com.jacadzaca.monopoly.gamelogic.Tile
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.slot
 import io.mockk.verify
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.*
@@ -17,11 +17,37 @@ internal class PlayerMovesTest {
   private val gameState = mockk<GameState>(relaxed = true)
   private val playersId = UUID.randomUUID()
   private val newPosition = Random.nextInt(1, Int.MAX_VALUE)
-  private val transformation = PlayerMoves(player, playersId, newPosition, gameState)
+  private val createPayment = mockk<(Player, UUID, Liability, GameState) -> (LiabilityPayment)>()
+  private val transformation = PlayerMoves(player, playersId, newPosition, gameState, createPayment)
+
+  @BeforeEach
+  fun setUp() {
+    val tileWithoutOwner = mockk<Tile>()
+    every { gameState.tiles[newPosition] } returns tileWithoutOwner
+    every { tileWithoutOwner.ownersId } returns null
+  }
 
   @Test
   fun `transform sets player's position to newPosition`() {
     transformation.transform()
     verify { player.setPosition(newPosition) }
   }
+
+  @Test
+  fun `transform makes player pay a liability if he steps on a tile owned by different player`() {
+    val tileOwnedByOther = mockk<Tile>()
+    every { tileOwnedByOther.totalRent() } returns Random.nextInt().toBigInteger()
+    every { tileOwnedByOther.ownersId } returns UUID.randomUUID()
+    every { tileOwnedByOther.ownersId == playersId } returns false
+    every { tileOwnedByOther.ownersId == null } returns false
+    every { gameState.players[tileOwnedByOther.ownersId] } returns mockk(name = "tile owner")
+    every { gameState.tiles[newPosition] } returns tileOwnedByOther
+    val liability = Liability(tileOwnedByOther.totalRent(), gameState.players[tileOwnedByOther.ownersId]!!, tileOwnedByOther.ownersId!!)
+    val rentPayment = mockk<LiabilityPayment>()
+    every { rentPayment.transform() } returns gameState
+    every { createPayment(player, playersId, liability, gameState) } returns rentPayment
+    transformation.transform()
+    verify { rentPayment.transform() }
+  }
+
 }
