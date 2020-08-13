6adef169fafbe5a5d2fb58e080c675ceca808c53
jaca <vitouejj@gmail.com>
2020-08-13 16:28:53 +0200

    renamme gameStatesId to gameRoomsId

--- src/main/kotlin/com/jacadzaca/monopoly/requests/RequestFactory.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/requests/RequestFactory.kt
@@ -3,8 +3,8 @@ package com.jacadzaca.monopoly.requests
 import java.util.*
 
 interface RequestFactory {
-  fun playerMoveRequest(playersId: UUID, gameStatesId: UUID): PlayerMovementRequest
-  fun tilePurchaseRequest(playersId: UUID, gameStatesId: UUID): TilePurchaseRequest
-  fun housePurchaseRequest(buyersId: UUID, gameStatesId: UUID): EstatePurchaseRequest
-  fun hotelPurchaseRequest(buyersId: UUID, gameStatesId: UUID): EstatePurchaseRequest
+  fun playerMoveRequest(playersId: UUID, gameRoomsId: UUID): PlayerMovementRequest
+  fun tilePurchaseRequest(playersId: UUID, gameRoomsId: UUID): TilePurchaseRequest
+  fun housePurchaseRequest(buyersId: UUID, gameRoomsId: UUID): EstatePurchaseRequest
+  fun hotelPurchaseRequest(buyersId: UUID, gameRoomsId: UUID): EstatePurchaseRequest
 }
