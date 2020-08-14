9da70dcaf9f3c36f8dc858ba92e18915aea5fb8e
jaca <vitouejj@gmail.com>
2020-08-14 17:33:29 +0200

    LiabilityPayment no longer use Liability

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/transformations/LiabilityPayment.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/transformations/LiabilityPayment.kt
@@ -1,25 +1,26 @@
 package com.jacadzaca.monopoly.gamelogic.transformations
 
-import com.jacadzaca.monopoly.gamelogic.GameState
-import com.jacadzaca.monopoly.gamelogic.Liability
-import com.jacadzaca.monopoly.gamelogic.Player
+import com.jacadzaca.monopoly.gamelogic.*
+import java.math.*
 import java.util.*
 
 class LiabilityPayment(
   private val payer: Player,
   private val payerId: UUID,
-  private val liability: Liability,
+  private val receiver: Player,
+  private val receiversId: UUID,
+  private val liability: BigInteger,
   private val target: GameState
 ) : Transformation() {
   override fun transform(): GameState {
-    return if (liability.amount > payer.balance) {
+    return if (liability > payer.balance) {
       target
-        .update(payerId, payer.detractFunds(liability.amount))
-        .update(liability.recevierId, liability.recevier.addFunds(payer.balance))
+        .update(payerId, payer.detractFunds(liability))
+        .update(receiversId, receiver.addFunds(payer.balance))
     } else {
       target
-        .update(payerId, payer.detractFunds(liability.amount))
-        .update(liability.recevierId, liability.recevier.addFunds(liability.amount))
+        .update(payerId, payer.detractFunds(liability))
+        .update(receiversId, receiver.addFunds(liability))
     }
   }
 }
--- src/test/kotlin/com/jacadzaca/monopoly/gamelogic/transformations/LiabilityPaymentTest.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/gamelogic/transformations/LiabilityPaymentTest.kt
@@ -11,41 +11,38 @@ internal class LiabilityPaymentTest {
   private val payer = mockk<Player>(relaxed = true)
   private val receiver = mockk<Player>(relaxed = true)
   private val payersId = UUID.randomUUID()
-  private val liability = mockk<Liability>()
+  private val receiversId = UUID.randomUUID()
   private val gameState = mockk<GameState>()
-  private val transformation = LiabilityPayment(payer, payersId, liability, gameState)
+  private val payersBalance = randomPositive().toBigInteger()
+  private val liability = payersBalance - BigInteger.ONE
+  private val transformation = LiabilityPayment(payer, payersId, receiver, receiversId, liability, gameState)
 
   @BeforeEach
   fun setUp() {
-    every { payer.balance } returns randomPositive().toBigInteger()
-    every { liability.recevier } returns receiver
-    every { liability.recevierId } returns UUID.randomUUID()
-    every { liability.amount } returns payer.balance - BigInteger.ONE
+    clearAllMocks()
+    every { payer.balance } returns payersBalance
     every { gameState.update(payersId, any()) } returns gameState
-    every { gameState.update(liability.recevierId, any()) } returns gameState
+    every { gameState.update(receiversId, any()) } returns gameState
   }
 
   @Test
   fun `transform detracts from the payer's balance`() {
     transformation.transform()
-    val amount = liability.amount
-    verify { payer.detractFunds(amount) }
+    verify { gameState.update(payersId, payer.detractFunds(liability)) }
   }
 
   @Test
   fun `transform adds the amount to the receiver's balance`() {
     transformation.transform()
-    val amount = liability.amount
-    verify { receiver.addFunds(amount) }
+    verify { gameState.update(receiversId, receiver.addFunds(liability)) }
   }
 
   @Test
   fun `transform only transfers what the payer has`() {
-    every { liability.amount } returns payer.balance + BigInteger.ONE
+    val liability = payer.balance + BigInteger.ONE
+    val transformation = LiabilityPayment(payer, payersId, receiver, receiversId, liability, gameState)
     transformation.transform()
-    val amount = liability.amount
-    val payersBalance = payer.balance
-    verify { receiver.addFunds(payersBalance) }
-    verify { payer.detractFunds(amount) }
+    verify { gameState.update(receiversId, receiver.addFunds(payersBalance)) }
+    verify { gameState.update(payersId, payer.detractFunds(liability)) }
   }
 }
