0389c4dbfff75d412b42be2f30063819405ea638
jaca <vitouejj@gmail.com>
2020-08-14 17:48:58 +0200

    remove Liability

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/Liability.kt
+++
@@ -1,6 +0,0 @@
-package com.jacadzaca.monopoly.gamelogic
-
-import java.math.BigInteger
-import java.util.*
-
-data class Liability(val amount: BigInteger, val recevier: Player, val recevierId: UUID)
