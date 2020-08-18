e064cb43565d24490069fde4292eb3e1eaf85cef
jaca <vitouejj@gmail.com>
2020-08-19 00:36:57 +0200

    rename PlayerMovementRequest.actionCreator to createMove

--- src/main/kotlin/com/jacadzaca/monopoly/requests/PlayerMovementRequest.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/requests/PlayerMovementRequest.kt
@@ -6,12 +6,12 @@ import com.jacadzaca.monopoly.requests.Request.Companion.invalidPlayerId
 import java.util.*
 
 class PlayerMovementRequest(
-    private val playersId: UUID,
-    private val actionCreator: (Player, UUID, GameState) -> MovePlayer,
-    private val context: GameState
+  private val playersId: UUID,
+  private val createMove: (Player, UUID, GameState) -> MovePlayer,
+  private val context: GameState
 ) : Request {
   override fun validate(): ValidationResult {
     val player = context.players[playersId] ?: return ValidationResult.Failure(invalidPlayerId)
-    return ValidationResult.Success(actionCreator(player, playersId, context))
+    return ValidationResult.Success(createMove(player, playersId, context))
   }
 }
