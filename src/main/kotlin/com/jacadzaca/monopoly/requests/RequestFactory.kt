e47fbfdd39d9bc366eda0fd0398688f4dc542485
jaca <vitouejj@gmail.com>
2020-08-19 00:24:46 +0200

    RequestFactory now takes in a playersId and a GameState

--- src/main/kotlin/com/jacadzaca/monopoly/requests/RequestFactory.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/requests/RequestFactory.kt
@@ -1,10 +1,11 @@
 package com.jacadzaca.monopoly.requests
 
+import com.jacadzaca.monopoly.gamelogic.*
 import java.util.*
 
 interface RequestFactory {
-  fun playerMoveRequest(playersId: UUID, gameRoomsId: UUID): PlayerMovementRequest
-  fun tilePurchaseRequest(playersId: UUID, gameRoomsId: UUID): TilePurchaseRequest
-  fun housePurchaseRequest(buyersId: UUID, gameRoomsId: UUID): EstatePurchaseRequest
-  fun hotelPurchaseRequest(buyersId: UUID, gameRoomsId: UUID): EstatePurchaseRequest
+  fun playerMoveRequest(playersId: UUID, gameState: GameState): PlayerMovementRequest
+  fun tilePurchaseRequest(playersId: UUID, gameState: GameState): TilePurchaseRequest
+  fun housePurchaseRequest(playersId: UUID, gameState: GameState): EstatePurchaseRequest
+  fun hotelPurchaseRequest(playersId: UUID, gameState: GameState): EstatePurchaseRequest
 }
