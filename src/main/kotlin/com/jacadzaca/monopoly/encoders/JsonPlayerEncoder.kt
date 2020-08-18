8fba341d1b34ed7a0da4625ec6a55d8e0dc85201
jaca <vitouejj@gmail.com>
2020-08-18 23:55:25 +0200

    remove JsonPlayerEncoder

--- src/main/kotlin/com/jacadzaca/monopoly/encoders/JsonPlayerEncoder.kt
+++
@@ -1,9 +0,0 @@
-package com.jacadzaca.monopoly.encoders
-
-import com.jacadzaca.monopoly.gamelogic.*
-import io.vertx.core.json.*
-
-object JsonPlayerEncoder : Encoder<Player, JsonObject> {
-  override fun encode(obj: Player): JsonObject =
-    JsonObject().put("balance", obj.toString()).put("position", obj.position)
-}
