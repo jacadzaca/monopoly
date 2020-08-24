3c5a2429d8270e325bb85b4111e3691ca29e3255
jaca <vitouejj@gmail.com>
2020-08-24 22:04:51 +0200

    Random.nextPositive() now has an until optional argument

--- src/test/kotlin/com/jacadzaca/monopoly/util.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/util.kt
@@ -2,4 +2,4 @@ package com.jacadzaca.monopoly
 
 import kotlin.random.*
 
-fun Random.nextPositive(from: Int = 1): Int = nextInt(from, Int.MAX_VALUE)
+fun Random.nextPositive(from: Int = 1, until: Int = Int.MAX_VALUE): Int = nextInt(from, until)
