361af64232d3fc7e766957e08448dedf9a28b50a
jaca <vitouejj@gmail.com>
2020-08-24 22:08:42 +0200

    add checks for Random.nextPositive()

--- src/test/kotlin/com/jacadzaca/monopoly/util.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/util.kt
@@ -2,4 +2,11 @@ package com.jacadzaca.monopoly
 
 import kotlin.random.*
 
-fun Random.nextPositive(from: Int = 1, until: Int = Int.MAX_VALUE): Int = nextInt(from, until)
+fun Random.nextPositive(from: Int = 1, until: Int = Int.MAX_VALUE): Int {
+  if (from > until) {
+    throw IllegalArgumentException("Random.nextPositive: until must be greater than until")
+  } else if (from <= 0) {
+    throw IllegalArgumentException("Random.nextPositive: from must be greater than zero")
+  }
+  return nextInt(from, until)
+}
