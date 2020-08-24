de1651d105fbf730effa957e0efdc2402a21790f
jaca <vitouejj@gmail.com>
2020-08-24 21:59:32 +0200

    Random.nextPositive() now has an optional argument form

--- src/test/kotlin/com/jacadzaca/monopoly/util.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/util.kt
@@ -2,4 +2,4 @@ package com.jacadzaca.monopoly
 
 import kotlin.random.*
 
-fun Random.nextPositive(): Int = nextInt(1, Int.MAX_VALUE)
+fun Random.nextPositive(from: Int = 1): Int = nextInt(from, Int.MAX_VALUE)
