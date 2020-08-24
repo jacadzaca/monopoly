ef831d145fb3504ac7f51fd7fa3fcb1ad921b6ec
jaca <vitouejj@gmail.com>
2020-08-24 22:13:52 +0200

    remove rendundant check from Random.nextPositive()

--- src/test/kotlin/com/jacadzaca/monopoly/util.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/util.kt
@@ -3,9 +3,7 @@ package com.jacadzaca.monopoly
 import kotlin.random.*
 
 fun Random.nextPositive(from: Int = 1, until: Int = Int.MAX_VALUE): Int {
-  if (from > until) {
-    throw IllegalArgumentException("Random.nextPositive: until must be greater than until")
-  } else if (from <= 0) {
+  if (from <= 0) {
     throw IllegalArgumentException("Random.nextPositive: from must be greater than zero")
   }
   return nextInt(from, until)
