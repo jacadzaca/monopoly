6c6d86249e8d99abac27172194f97dbc6f3b8317
jaca <vitouejj@gmail.com>
2020-08-27 18:07:28 +0200

    add UUIDSerializer

---
+++ src/main/kotlin/com/jacadzaca/monopoly/marshallers/UUIDSerializer.kt
@@ -0,0 +1,14 @@
+package com.jacadzaca.monopoly.marshallers
+
+import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.*
+import kotlinx.serialization.encoding.*
+import java.util.*
+
+object UUIDSerializer : KSerializer<UUID> {
+  override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(UUID::class.simpleName!!, PrimitiveKind.STRING)
+
+  override fun deserialize(decoder: Decoder): UUID = UUID.fromString(decoder.decodeString())
+
+  override fun serialize(encoder: Encoder, value: UUID) = encoder.encodeString(value.toString())
+}
