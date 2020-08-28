cf32c2f3f9f07bf88d863ffebcc97a3c7118a8a0
jaca <vitouejj@gmail.com>
2020-08-28 18:31:45 +0200

    replace createHouses with an inline

--- src/test/kotlin/com/jacadzaca/monopoly/serializers/PersistentEstateListSerializerTest.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/serializers/PersistentEstateListSerializerTest.kt
@@ -1,12 +1,19 @@
 package com.jacadzaca.monopoly.serializers
 
 import com.jacadzaca.monopoly.*
+import com.jacadzaca.monopoly.gamelogic.*
+import kotlinx.collections.immutable.*
 import kotlinx.serialization.json.*
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
+import kotlin.random.*
 
 internal class PersistentEstateListSerializerTest {
-  private val estates = createHouses()
+  private val estates =
+    (1..10)
+      .map { Estate.House(Random.nextPositive().toBigInteger(), Random.nextPositive().toBigInteger()) }
+      .plus((1..10).map { Estate.Hotel(Random.nextPositive().toBigInteger(), Random.nextPositive().toBigInteger()) })
+      .toPersistentList()
 
   @Test
   fun `decode method is the inverse of the encode method`() {
--- src/test/kotlin/com/jacadzaca/monopoly/util.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/util.kt
@@ -1,7 +1,5 @@
 package com.jacadzaca.monopoly
 
-import com.jacadzaca.monopoly.gamelogic.*
-import kotlinx.collections.immutable.*
 import kotlin.random.*
 
 fun Random.nextPositive(from: Int = 1, until: Int = Int.MAX_VALUE): Int {
@@ -9,10 +7,4 @@ fun Random.nextPositive(from: Int = 1, until: Int = Int.MAX_VALUE): Int {
     throw IllegalArgumentException("Random.nextPositive: from must be greater than zero")
   }
   return nextInt(from, until)
-}
-
-fun createHouses(): PersistentList<Estate> {
-  return (1..10)
-    .map { Estate.House(Random.nextPositive().toBigInteger(), Random.nextPositive().toBigInteger()) }
-    .toPersistentList()
 }
