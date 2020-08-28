37a0846bea1b3f1fd4a3ec542947b1d6019c6cc9
jaca <vitouejj@gmail.com>
2020-08-28 18:07:29 +0200

    remove Marshallers

--- src/main/kotlin/com/jacadzaca/monopoly/marshallers/json.kt
+++
@@ -1,8 +0,0 @@
-package com.jacadzaca.monopoly.marshallers
-
-import io.vertx.core.json.*
-
-@Suppress("UNCHECKED_CAST")
-fun <T> JsonObject.getJsonArrayOfType(key: String): List<T> {
-  return getJsonArray(key).map { it as T }
-}
