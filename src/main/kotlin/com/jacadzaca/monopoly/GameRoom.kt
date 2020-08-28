61f52a0bf53078c08f58d9f4cb77241aa1d93323
jaca <vitouejj@gmail.com>
2020-08-28 18:26:33 +0200

    GameRoom is now Serializable

--- src/main/kotlin/com/jacadzaca/monopoly/GameRoom.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/GameRoom.kt
@@ -3,7 +3,9 @@ package com.jacadzaca.monopoly
 import com.jacadzaca.monopoly.gamelogic.*
 import io.vertx.core.buffer.*
 import io.vertx.core.shareddata.impl.*
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class GameRoom(val gameState: GameState, val version: Long = 0L) : ClusterSerializable {
   override fun readFromBuffer(pos: Int, buffer: Buffer): Int {
     throw UnsupportedOperationException()
