d185852cea2c4453e652c5ef5761c39af6ec4c3d
jaca <vitouejj@gmail.com>
2020-08-28 20:31:58 +0200

    GameRoom implements ClusterSerializable

--- src/main/kotlin/com/jacadzaca/monopoly/GameRoom.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/GameRoom.kt
@@ -7,14 +7,38 @@ import com.jacadzaca.monopoly.serializers.*
 import io.vertx.core.buffer.*
 import io.vertx.core.shareddata.impl.*
 import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 
+/**
+ * To create an instance use the constructor with two arguments [constructor(gameState: GameState, version: Long)].
+ * The zero-argument constructor is defined to provide compliance with the [ClusterSerializable] interface
+ */
 @Serializable
-data class GameRoom(val gameState: GameState, val version: Long = 0L) : ClusterSerializable {
+class GameRoom constructor() : ClusterSerializable {
+  private lateinit var _gameState: GameState
+  private var _version: Long = 0L
+
+  constructor(gameState: GameState, version: Long) {
+    _gameState = gameState
+    _version = version
+  }
+
+  val gameState
+    get() = _gameState
+  val version
+    get() = _version
+
   override fun readFromBuffer(pos: Int, buffer: Buffer): Int {
-    throw UnsupportedOperationException()
+    val gameRoom = Json.decodeFromString<GameRoom>(buffer.getString(pos, buffer.length()))
+    this._gameState = gameRoom._gameState
+    this._version = gameRoom._version
+    // no clue why it is required to return this. Shouldn't I change the return statement to [pos + buffer.length()]?
+    return pos
   }
 
   override fun writeToBuffer(buffer: Buffer) {
-    throw UnsupportedOperationException()
+    buffer.appendString(Json.encodeToString(this))
   }
+
+
 }
