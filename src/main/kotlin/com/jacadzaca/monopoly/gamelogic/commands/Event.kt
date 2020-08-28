f56997436dbe8f53858a05c91dfd0fbaf1ac2e6c
jaca <vitouejj@gmail.com>
2020-08-28 18:23:07 +0200

    Event is now Serializable

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/commands/Event.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/commands/Event.kt
@@ -1,12 +1,21 @@
+@file:UseSerializers(BigIntegerSerializer::class, UUIDSerializer::class)
+
 package com.jacadzaca.monopoly.gamelogic.commands
 
 import com.jacadzaca.monopoly.gamelogic.*
+import com.jacadzaca.monopoly.serializers.*
+import kotlinx.serialization.*
 import java.math.*
 import java.util.*
 
+@Serializable
 sealed class Event {
+  @Serializable
   data class PlayerMoved(val playersId: UUID, val newPosition: Int) : Event()
+  @Serializable
   data class TilePurchased(val buyersId: UUID, val purchasedTilesIndex: Int) : Event()
+  @Serializable
   data class LiabilityPaid(val payersId: UUID, val receiversId: UUID, val liability: BigInteger) : Event()
+  @Serializable
   data class EstatePurchased(val buyersId: UUID, val tileIndex: Int, val purchasedEstate: Estate) : Event()
 }
