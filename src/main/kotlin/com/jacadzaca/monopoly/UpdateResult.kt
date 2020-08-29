dd89cdd4968143729ca9efcb27645e00e0274558
jaca <vitouejj@gmail.com>
2020-08-29 15:48:56 +0200

    UpdateResult is now Serializable

--- src/main/kotlin/com/jacadzaca/monopoly/UpdateResult.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/UpdateResult.kt
@@ -1,6 +1,11 @@
 package com.jacadzaca.monopoly
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 sealed class UpdateResult {
+  @Serializable
   object Success : UpdateResult()
+  @Serializable
   data class Failure(val reason: String) : UpdateResult()
 }
