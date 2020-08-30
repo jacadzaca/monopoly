6ebcfc4794afdbe2d43c06234df0332c6d8bbe84
jaca <vitouejj@gmail.com>
2020-08-30 16:24:14 +0200

    refactor GameRoomLookupVerticleTest

--- src/test/kotlin/com/jacadzaca/monopoly/GameRoomLookupVerticleTest.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/GameRoomLookupVerticleTest.kt
@@ -10,13 +10,12 @@ import kotlinx.coroutines.*
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.extension.*
+import kotlin.random.*
 
 @ExtendWith(VertxExtension::class)
 internal class GameRoomLookupVerticleTest {
-  private val room = mockk<GameRoom>(name = "room")
-  private val room1 = mockk<GameRoom>(name = "room1")
-  private val roomName = "test"
-  private val roomName1 = "${roomName}1"
+  private val room = mockk<GameRoom>()
+  private val roomName = Random.nextString()
 
   @BeforeEach
   fun setUp(vertx: Vertx) {
@@ -24,23 +23,22 @@ internal class GameRoomLookupVerticleTest {
       vertx.deployVerticleAwait(GameRoomLookupVerticle())
       val rooms = vertx.sharedData().getLocalAsyncMapAwait<String, GameRoom>("game-rooms")
       rooms.putAwait(roomName, room)
-      rooms.putAwait(roomName1, room1)
     }
   }
 
   @Test
+  // repeat in ordedr to check if the verticle responds to multiple requests
+  @RepeatedTest(2)
   fun `verticle replies with the room if there is an entry under name it exists`(vertx: Vertx) {
     runBlocking {
-      assertEquals(room, vertx.eventBus().requestAwait<GameRoom>(GameRoomLookupVerticle.ADDRESS, roomName).body())
-      assertEquals(room1, vertx.eventBus().requestAwait<GameRoom>(GameRoomLookupVerticle.ADDRESS, roomName1).body())
+      assertSame(room, vertx.eventBus().requestAwait<GameRoom>(GameRoomLookupVerticle.ADDRESS, roomName).body())
     }
   }
 
   @Test
   fun `verticle replies with null if there is no room under given name`(vertx: Vertx) {
     runBlocking {
-      assertEquals(null, vertx.eventBus().requestAwait<GameRoom>(GameRoomLookupVerticle.ADDRESS, "invalidName").body())
-      assertEquals(null, vertx.eventBus().requestAwait<GameRoom>(GameRoomLookupVerticle.ADDRESS, "invalidName1").body())
+      assertSame(null, vertx.eventBus().requestAwait<GameRoom>(GameRoomLookupVerticle.ADDRESS, "invalidName").body())
     }
   }
 }
