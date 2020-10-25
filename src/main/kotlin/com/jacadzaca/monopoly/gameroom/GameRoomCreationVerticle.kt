eaa49d6e7ef51e92acb81bc5f2366ce17651a504
jaca <vitouejj@gmail.com>
2020-10-25 19:17:17 +0100

    add a verticle for creating game rooms

---
+++ src/main/kotlin/com/jacadzaca/monopoly/gameroom/GameRoomCreationVerticle.kt
@@ -0,0 +1,38 @@
+package com.jacadzaca.monopoly.gameroom
+
+import io.vertx.core.impl.logging.*
+import io.vertx.kotlin.core.shareddata.*
+import io.vertx.kotlin.coroutines.*
+import kotlinx.coroutines.*
+
+class GameRoomCreationVerticle : CoroutineVerticle() {
+  companion object {
+    const val ADDRESS = "create-game-room"
+    internal const val ROOMS_NAME = "roomsName"
+    internal const val SUCCESS = 0
+    internal const val NAME_TAKEN = 1
+    private val logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  override suspend fun start() {
+    val rooms = vertx.sharedData()
+      .getLocalAsyncMapAwait<String, GameRoom>("game-rooms")
+    val messages = vertx.eventBus()
+      .registerCodec(GameRoomCodec)
+      .consumer<GameRoom>(ADDRESS)
+      .toChannel(vertx)
+    launch {
+      for (message in messages) {
+        launch {
+          val response = rooms.putIfAbsentAwait(message.headers()[ROOMS_NAME], message.body())
+          if (response == null) {
+            message.reply(SUCCESS)
+          } else {
+            message.reply(NAME_TAKEN)
+          }
+        }
+      }
+    }
+    logger.info("Started a ${this::class.qualifiedName} instance")
+  }
+}
---
+++ src/test/kotlin/com/jacadzaca/monopoly/gameroom/GameRoomCreationVerticleTest.kt
@@ -0,0 +1,74 @@
+package com.jacadzaca.monopoly.gameroom
+
+import com.jacadzaca.monopoly.*
+import io.mockk.*
+import io.vertx.core.Vertx
+import io.vertx.core.shareddata.*
+import io.vertx.junit5.*
+import io.vertx.junit5.Timeout
+import io.vertx.kotlin.core.*
+import io.vertx.kotlin.core.eventbus.*
+import io.vertx.kotlin.core.shareddata.*
+import kotlinx.coroutines.*
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.extension.*
+import java.util.concurrent.*
+import kotlin.random.*
+
+@ExtendWith(VertxExtension::class)
+@Timeout(value = 5, timeUnit = TimeUnit.SECONDS)
+internal class GameRoomCreationVerticleTest {
+  private val room = mockk<GameRoom>()
+  private val roomsId = Random.nextString()
+  private lateinit var rooms: AsyncMap<String, GameRoom>
+  private var isDeployed = false
+
+  @BeforeEach
+  fun setUp(vertx: Vertx) {
+    every { room.version } returns Random.nextPositive().toLong()
+    //every { room.gameState } returns mockk()
+    if (!isDeployed) {
+      runBlocking {
+        vertx.deployVerticleAwait(GameRoomCreationVerticle())
+        rooms = vertx.sharedData().getLocalAsyncMapAwait("game-rooms")
+        isDeployed = true
+      }
+    }
+    rooms.clear()
+  }
+
+  @Test
+  fun `verticle saves the game room under given id`(vertx: Vertx) {
+    runBlocking {
+      saveRoom(vertx, room)
+      assertSame(room, rooms.getAwait(roomsId))
+    }
+  }
+
+  @Test
+  fun `verticle replies with success if room was saved`(vertx: Vertx) {
+    runBlocking {
+      assertEquals(GameRoomCreationVerticle.SUCCESS, saveRoom(vertx, room))
+    }
+  }
+
+  @Test
+  fun `verticle replies with failure if rooms with desired name already exists`(vertx: Vertx) {
+    runBlocking {
+      saveRoom(vertx, room)
+      assertEquals(GameRoomCreationVerticle.NAME_TAKEN, saveRoom(vertx, room))
+    }
+  }
+
+  private suspend fun saveRoom(vertx: Vertx, room: GameRoom) =
+    vertx.eventBus()
+      .requestAwait<Int>(
+        GameRoomCreationVerticle.ADDRESS,
+        room,
+        deliveryOptionsOf()
+          .addHeader(GameRoomCreationVerticle.ROOMS_NAME, roomsId)
+          .setCodecName(GameRoomCodec.name())
+      )
+      .body()
+}
