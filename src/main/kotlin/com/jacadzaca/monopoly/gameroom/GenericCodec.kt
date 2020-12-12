d8cefa91d6ddae50a678f25e17740b4be5444e06
jaca <vitouejj@gmail.com>
2020-12-12 23:49:31 +0100

    GenericCodecs now have names, add methods for creating generic codecs for Computations

--- src/main/kotlin/com/jacadzaca/monopoly/gameroom/GenericCodec.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gameroom/GenericCodec.kt
@@ -1,11 +1,32 @@
 package com.jacadzaca.monopoly.gameroom
 
+import com.jacadzaca.monopoly.*
 import io.vertx.core.buffer.*
 import io.vertx.core.eventbus.*
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
+import java.util.*
+import kotlin.reflect.*
 
-class GenericCodec<T>(private val serializer: KSerializer<T>) : MessageCodec<T, T> {
+class GenericCodec<T>(
+  private val serializer: KSerializer<T>,
+  private val name: String = UUID.randomUUID().toString()
+) : MessageCodec<T, T> {
+  companion object {
+    @JvmStatic
+    fun <T> computationCodec(
+      serializer: KSerializer<T>,
+      type: KClass<*>
+    ): MessageCodec<Computation<T>, Computation<T>> {
+      return GenericCodec(Computation.serializer(serializer), computationCodecName(type))
+    }
+
+    @JvmStatic
+    fun computationCodecName(type: KClass<*>): String {
+      return "${Computation::class.simpleName}${type.simpleName}"
+    }
+  }
+
   override fun encodeToWire(buffer: Buffer, s: T) {
     buffer.appendString(Json.encodeToString(serializer, s))
   }
@@ -16,7 +37,7 @@ class GenericCodec<T>(private val serializer: KSerializer<T>) : MessageCodec<T, 
 
   override fun transform(s: T): T = s
 
-  override fun name(): String = "${this::class.simpleName} ${serializer::class.simpleName}"
+  override fun name(): String = name
 
   override fun systemCodecID(): Byte = -1
 }
