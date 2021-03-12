edb888afcb0319efa79942ef44e8a4ac37e62a02
jaca <vitouejj@gmail.com>
2021-03-12 19:55:04 +0100

    add change turn command

--- src/main/kotlin/com/jacadzaca/monopoly/gamelogic/commands/ChangeTurn.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/gamelogic/commands/ChangeTurn.kt
@@ -6,7 +6,11 @@ class ChangeTurn(private val target: GameState) : Command {
   override fun asEvent(): Event = Event.TurnChanged
 
   override fun execute(): GameState {
-    return target
-      .copy(currentTurn = (target.currentTurn + 1) % target.turnOrder.size)
+    return if (target.turnOrder.size == 0) {
+      target.copy(currentTurn = 0)
+    } else {
+      target
+        .copy(currentTurn = (target.currentTurn + 1) % target.turnOrder.size)
+    }
   }
 }
