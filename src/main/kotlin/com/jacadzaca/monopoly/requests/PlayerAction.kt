990b7b97b18194c0760f185d97ee9ff7e673f88a
jaca <vitouejj@gmail.com>
2021-04-05 20:06:42 +0200

    make PlayerAction @Serializable

--- src/main/kotlin/com/jacadzaca/monopoly/requests/PlayerAction.kt
+++ src/main/kotlin/com/jacadzaca/monopoly/requests/PlayerAction.kt
@@ -1,13 +1,19 @@
 package com.jacadzaca.monopoly.requests
 
-import kotlinx.serialization.Serializable
+import kotlinx.serialization.*
 
 @Serializable
 enum class PlayerAction {
+  @SerialName("move")
   MOVE,
+  @SerialName("buy-tile")
   BUY_TILE,
+  @SerialName("buy-house")
   BUY_HOUSE,
+  @SerialName("buy-hotel")
   BUY_HOTEL,
+  @SerialName("join")
   JOIN,
+  @SerialName("leave")
   LEAVE,
 }
