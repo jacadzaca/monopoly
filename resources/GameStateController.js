61091d7fe29aba4bbeccd37a937029b5136e2180
jaca <vitouejj@gmail.com>
2021-04-18 19:49:48 +0200

    client now handles new protocol

--- resources/GameStateController.js
+++ resources/GameStateController.js
@@ -35,33 +35,42 @@ socket.onmessage = event => {
 const handleRequest = json => {
     console.log(json);
     switch(json.type) {
-        case 'player-joined':
-            gameState.players[json.playersId] = {position: 0, balance: 1000};
+        case 'playerJoin':
+            gameState.players[json.playersId] = json.newPlayer;
             gameState.turnOrder.push(json.playersId);
             window.dispatchEvent(createEvent('x-update-list', gameState.players));
             break;
-        case 'player-left':
+        case 'playerLeave':
             delete gameState.players[json.playersId];
             gameState.turnOrder = removeElement(gameState.turnOrder, json.playersId);
             window.dispatchEvent(createEvent('x-update-list', gameState.players));
             break;
-        case 'turn-change':
-            gameState.currentTurn += 1;
-            gameState.currentTurn %= gameState.turnOrder.length;
-            const currentPlayer = gameState.turnOrder[gameState.currentTurn]
-            window.dispatchEvent(createEvent('x-turn-change', currentPlayer))
+        case 'turnChange':
+            const currentPlayer = gameState.turnOrder[json.turn];
+            window.dispatchEvent(createEvent('x-turn-change', currentPlayer));
             break;
-        case 'moved':
+        case 'positionChange':
             gameState.players[json.playersId].position = json.newPosition;
             window.dispatchEvent(createEvent('x-update-list', gameState.players));
             break;
-        case 'liability-paid':
-            const liability = BigInt(json.liability)
-            const payersBalance = BigInt(gameState.players[json.payersId].balance)
-            gameState.players[json.payersId].balance = payersBalance - liability;
-            const reciversBalance = BigInt(gameState.players[json.receiversId].balance)
-            gameState.players[json.receiversId].balance = reciversBalance + liability;
+        case 'balanceChange':
+            gameState.players[json.playersId].balance = json.newBalance;
             window.dispatchEvent(createEvent('x-update-list', gameState.players));
+            break;
+        case 'tileOwnershipChange':
+            gameState.tiles[json.tilesIndex].ownersId = json.newOwner;
+            window.dispatchEvent(createEvent('x-update-tile', gameState.tiles));
+            break;
+        case 'estateAdded':
+            switch(json.estate.type) {
+                case 'house':
+                    gameState.tiles[json.tilesIndex].houses.push(json.estate);
+                    break;
+                case 'hotel':
+                    gameState.tiles[json.tilesIndex].hotels.push(json.estate);
+                    break;
+            }
+            window.dispatchEvent(createEvent('x-update-tile', gameState.tiles));
             break;
         default:
             break;
