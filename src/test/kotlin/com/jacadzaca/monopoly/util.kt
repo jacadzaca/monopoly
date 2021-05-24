745aaaca6b79c647d760ae8b7b2b11d56322a772
jaca <vitouejj@gmail.com>
2021-05-24 18:14:07 +0200

    update dependencies

--- src/test/kotlin/com/jacadzaca/monopoly/util.kt
+++ src/test/kotlin/com/jacadzaca/monopoly/util.kt
@@ -10,7 +10,7 @@ fun Random.nextPositive(from: Int = 1, until: Int = Int.MAX_VALUE): Int {
 }
 
 fun Random.nextString(): String {
-    return generateSequence { nextInt(Char.MIN_VALUE.toInt(), Char.MAX_VALUE.toInt()) }
+    return generateSequence { nextInt(Char.MIN_VALUE.code, Char.MAX_VALUE.code) }
         .map(Int::toChar)
         .take(200)
         .toString()
